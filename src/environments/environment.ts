552d714428809731cec55950905aa43f370b77da
sebas-meseta <sebas.ao@outlook.com>
2024-01-04 17:28:29 -0500

    Firebase instalado y archivo environment creado, falta importar firebase en app.config

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBohlOZId6HBMm7eIPXZ5kUm0ftSOSNmCg",
+        authDomain: "salarcon-it.firebaseapp.com",
+        projectId: "salarcon-it",
+        storageBucket: "salarcon-it.appspot.com",
+        messagingSenderId: "133262156036",
+        appId: "1:133262156036:web:937354a6963a62bac97051",
+        measurementId: "G-DT7DW974TQ"
+    }
+  };

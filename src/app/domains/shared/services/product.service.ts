9680fcaf8a9fd80fc2f3021fddcba3a8223e6162
sebas-meseta <sebas.ao@outlook.com>
2024-01-05 16:31:51 -0500

    Module ProvideHttpClient importado y dependecia injectada en service Product

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  private http = inject(HttpClient)
 
   constructor() { }
 }

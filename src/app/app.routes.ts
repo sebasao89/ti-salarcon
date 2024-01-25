6493661ffc338e7dbd2772bd9c055f0bd8d250e9
sebas-meseta <sebas.ao@outlook.com>
2024-01-25 16:53:34 -0500

    Routing de detalle de cada producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NoFoundComponent } from './domains/info/pages/no-found/no-found.component';
 import { LayoutComponent } from './domains/shared/components/layout/layout.component';
 import { HomeComponent } from './domains/home/pages/home/home.component';
+import { ProductDetailComponent } from './domains/products/pages/product-detail/product-detail.component';
 
 
 export const routes: Routes = [
@@ -18,6 +19,10 @@ export const routes: Routes = [
             {
                 path: 'products',
                 component: ListComponent
+            },
+            {
+                path: 'products/:id',
+                component: ProductDetailComponent
             },
             {
                 path: 'about',
--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.model';
 import { TimeAgoPipe } from '../../../shared/pipes/time-ago.pipe';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, TimeAgoPipe],
+  imports: [CommonModule, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
